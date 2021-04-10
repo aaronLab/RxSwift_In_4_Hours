96fb5bc9435ff1d0265bf39f54cf661550c9f319
Aaron Lee <aaronlab.net@gmail.com>
2021-04-10 10:46:22 +0900

    key + myobservable

--- Example/season2/step1/RxSwift+MVVM/ViewController.swift
+++ Example/season2/step1/RxSwift+MVVM/ViewController.swift
@@ -10,19 +10,33 @@ import RxSwift
 import SwiftyJSON
 import UIKit
 
-let MEMBER_LIST_URL = "https://my.api.mockaroo.com/members_with_avatar.json?key=44ce18f0"
+let MEMBER_LIST_URL = "https://my.api.mockaroo.com/members_with_avatar.json?key=\(APIKey)"
+
+class MyObservable<T> {
+    
+    private let task: (@escaping (T) -> Void) -> Void
+    
+    init(task: @escaping (@escaping (T) -> Void) -> Void) {
+        self.task = task
+    }
+    
+    func subscribe(_ f: @escaping (T) -> Void) {
+        task(f)
+    }
+    
+}
 
 class ViewController: UIViewController {
     @IBOutlet var timerLabel: UILabel!
     @IBOutlet var editView: UITextView!
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true) { [weak self] _ in
             self?.timerLabel.text = "\(Date().timeIntervalSince1970)"
         }
     }
-
+    
     private func setVisibleWithAnimation(_ v: UIView?, _ s: Bool) {
         guard let v = v else { return }
         UIView.animate(withDuration: 0.3, animations: { [weak v] in
@@ -31,20 +45,38 @@ class ViewController: UIViewController {
             self?.view.layoutIfNeeded()
         })
     }
-
+    
+    func downloadJson(_ url: String) -> MyObservable<String?> {
+        
+        return MyObservable { f in
+            
+            DispatchQueue.global().async {
+                let url = URL(string: url)!
+                let data = try! Data(contentsOf: url)
+                let json = String(data: data, encoding: .utf8)
+                DispatchQueue.main.async {
+                    f(json)
+                }
+            }
+            
+        }
+        
+    }
+    
     // MARK: SYNC
-
+    
     @IBOutlet var activityIndicator: UIActivityIndicatorView!
-
+    
     @IBAction func onLoad() {
         editView.text = ""
-        setVisibleWithAnimation(activityIndicator, true)
-
-        let url = URL(string: MEMBER_LIST_URL)!
-        let data = try! Data(contentsOf: url)
-        let json = String(data: data, encoding: .utf8)
-        self.editView.text = json
+        self.setVisibleWithAnimation(self.activityIndicator, true)
         
-        self.setVisibleWithAnimation(self.activityIndicator, false)
+        let json: MyObservable<String?> = downloadJson(MEMBER_LIST_URL)
+        
+        json.subscribe { json in
+            self.editView.text = json
+            self.setVisibleWithAnimation(self.activityIndicator, false)
+        }
+        
     }
 }
